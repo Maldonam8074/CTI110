982bc6505dea84e0d175bf93af0e0daf6cf038e5
Maldonam8074 <31493796+Maldonam8074@users.noreply.github.com>
2017-08-30 20:31:12 -0400

    Add files via upload
    First submission

---
+++ M2T1_SalesPrediction_Maldonado.py
@@ -0,0 +1,34 @@
+# CTI-110
+# M2T1-Sales Prediction
+# Manuel Maldonado
+# 08/30/2017
+
+# Suggested Variables: totalSales, annualProfit
+
+# ask the user for total sales
+totalSales = int(input("What were your sales?: "))
+
+# calculate the profit amount
+annualProfit = 0.23 * totalSales
+
+# print the profit amount
+print ("the annual profit is", annualProfit)
+
+
+
+'''
+# program to add two numbers
+#userTyped = input("Type a number: ")
+#userNumber = int(userTyped)
+
+# get the numbers
+firstNumber = int(input("Type a number: "))
+secondNumber = int(input("Type another number: "))
+
+# add them
+answer = firstNumber + secondNumber
+
+
+# print the result
+print ("The answer is", answer)
+'''
