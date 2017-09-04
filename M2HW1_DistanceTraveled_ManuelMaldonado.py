95c8c02a0fa9192ea5130b1a56316cf785ddb4b3
Maldonam8074 <31493796+Maldonam8074@users.noreply.github.com>
2017-09-04 14:15:49 -0400

    Add files via upload
    Distance Traveled Formula

---
+++ M2HW1_DistanceTraveled_ManuelMaldonado.py
@@ -0,0 +1,32 @@
+#CTI-110
+#M2HW1 - Distance Traveled
+#Manuel Maldonado
+#09/04/2017
+#
+
+#distance = speed * time
+
+speed = 70
+
+#distanceAfter6
+
+#distanceAfter10
+
+#distanceAfter15
+
+distanceAfter6 = 6
+
+distanceAfter10 = 10
+
+distanceAfter15 = 15
+
+print ("A car is traveling at 70MPH")
+
+print ("The distance the car will travel in 6 hours is: ", (speed * distanceAfter6), "Miles")
+
+print ("The distance the car will travel in 10 hours is: ", (speed * distanceAfter10), "Miles")
+
+print ("The distance the car will travel in 15 hours is: ", (speed * distanceAfter15), "Miles")
+
+
+
