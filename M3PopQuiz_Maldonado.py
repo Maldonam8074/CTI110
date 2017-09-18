d9db4a5b83c9287dec4c984cb9059d0a73a375b4
Maldonam8074 <31493796+Maldonam8074@users.noreply.github.com>
2017-09-18 10:47:50 -0400

    Add files via upload
    Program that asks user two numbers then averages them.

---
+++ M3PopQuiz_Maldonado.py
@@ -0,0 +1,16 @@
+# CTI-110
+# M3 Pop Quiz
+# Manuel Maldonado
+# 09/18/2017
+
+# ask user for two numbers
+# average them
+# print result
+
+firstNumber = float(input('Please enter the First number? '))
+
+secondNumber = float(input('Please enter the Second Number? '))
+
+average = firstNumber + secondNumber
+
+print("The average is: ", format(average/2, '.0f'))
